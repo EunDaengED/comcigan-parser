4d871942c93adac67e2e664aa868379917153e10
EunDaeng <55231698+EunDaengED@users.noreply.github.com>
2020-12-10 09:30:06 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 require("./index.js");
+const sleep = require("http");
+
+/* Prevent Sleep in Heroku Server */
+setInterval(function () {
+	sleep.get("http://yachoo.herokuapp.com");
+}, 600000); // every 10 minutes
